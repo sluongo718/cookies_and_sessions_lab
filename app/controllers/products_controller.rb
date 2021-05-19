3a3c18728253c219f6d95699f6448ed83b0c3813
sluongo718 <sluongo718@yahoo.com>
2021-05-19 18:25:03 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,11 @@
 class ProductsController < ApplicationController
 
     def index 
-       
+
     end
 
     def add
         session[:cart] = params[:product]
+        redirect_to :root
     end
 end
